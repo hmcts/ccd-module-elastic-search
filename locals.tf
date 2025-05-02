bcfebd3837039df9ef801e683b7c6f1eb094d650
Chirag Pareek <chirag.pareek@justice.gov.uk>
2025-05-02 11:31:25 +0100

    change scope of the rbac

--- locals.tf
+++ locals.tf
@@ -4,13 +4,13 @@ locals {
 
   rbac_config = {
     "standard" = {
-      scope                = "/subscriptions/${data.azurerm_subscription.current.subscription_id}/resourceGroups/${var.vm_resource_group}"
+      scope                = module.virtual-machines.vm_id
       role_definition_name = "Virtual Machine User Login"
       principal_id         = var.env == "prod" || var.env == "production" ? "13dcb358-2b32-4afe-8d61-7889dc7dd39e" : "6a235ef2-26b8-4e52-a9fa-154091ca19d0" #DTS Production CCD Access for Users   / DTS Non-Production CCD Access for Users 
 
     },
     "sudo" = {
-      scope                = "/subscriptions/${data.azurerm_subscription.current.subscription_id}/resourceGroups/${var.vm_resource_group}"
+      scope                = module.virtual-machines.vm_id
       role_definition_name = "Virtual Machine Administrator Login"
       principal_id         = var.env == "prod" || var.env == "production" ? "219a0bcd-32e6-41df-bd3e-ec830500be98" : "975eb5f1-1fbf-4359-b7c3-8f4db3569351" # DTS Production CCD Access for Administrators /  DTS Non-Production CCD Access for Administrators
 
